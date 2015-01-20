ccfb8e259e8dcff675d41802e6ac6335633a88bd
Marcos Mariscal <cti.mmariscal@gmail.com>
2015-01-20 16:54:49 +0100

    Variables.php
    Pruebas con variables

---
+++ proyecto1/variables.php
@@ -0,0 +1,18 @@
+<?php
+
+$name = "Marcos";
+
+echo gettype($name);
+
+echo "<hr/>";
+
+var_dump($name);
+
+echo "<hr/>";
+
+echo is_string($name);
+
+echo "<hr/>";
+
+echo "<pre>";
+print_r($name);
