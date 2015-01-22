aeaf7622d6f447f59447201c278ba0c83394ad9a
Marcos Mariscal <cti.mmariscal@gmail.com>
2015-01-22 15:57:09 +0100

    Ficheros Formularios
    Lo de la tarde al final

--- formularios/validationForm.php
+++ formularios/validationForm.php
@@ -9,5 +9,34 @@
  */
 function validationForm($form, $filterdata)
 {
+    $validation=array();
+    
+    foreach($form as $key => $value)
+    {
+        if(isset($value['validation']) && is_array($value['validation']))
+            foreach($value['validation'] as $key2 => $value2)
+            {
+                switch ($key2)
+                {
+                    case 'required':
+                        if ($value['validation']['required']===TRUE)
+                        if(!isset($filterdata[$key]) or $filterdata[$key]=='')
+                        {
+                            if(isset($value['validation']['error_message']))
+                                $validation[$key]=$value['validation']['error_message'];
+                            else
+                                $validation[$key]='ERROR';
+                        }
+                            
+                            
+                    break;
+                }
+            }
+    }
+    if(empty($validation))
+        return TRUE;
     return $validation;
 }
+
+
+    
